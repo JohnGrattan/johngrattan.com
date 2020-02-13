2342c4f09b8c17d70b4a212699c3d7ec5882ac8c
John Grattan <jgrattan23@gmail.com>
2020-02-13 15:05:37 -0500

    Updated seo.js, added structured data to be tested

--- src/components/seo.js
+++ src/components/seo.js
@@ -27,20 +27,37 @@ function SEO({ description, lang, meta, title }) {
 
   const metaDescription = description || site.siteMetadata.description;
 
-  const structuredData = {
-    '@context': 'http://schema.org',
-    '@type': 'LocalBusiness',
-    name: 'John Grattan SEO & Web Design',
-    telephone: '(401) 216-9868',
-    email: 'contact@johngrattan.com',
-    address: {
-      '@type': 'PostalAddress',
-      streetAddress: '30 Roosevelt Road',
-      addressLocality: 'Plymouth',
-      addressRegion: 'MA',
-      postalCode: '02360',
+  const structuredDataLocalBusiness = `{
+    "@context": "http://schema.org",
+    "@type": "LocalBusiness",
+    "name": "John Grattan SEO & Web Design",
+    "priceRange": "$100 - $500",
+    "description": "John Grattan SEO & Web Design offers affordable Digital Marketing services such as SEO, SEM, Website Design and Web Development to small businesses throughout Massachusetts, from Boston to Plymouth."
+    "image":
+      "https://johngrattan.com/static/8cad32f9172e09f0099b1a68e49be910/bc59e/john-grattan-circle-headshot-green.png",
+    "telephone": "+1-401 216 9868",
+    "email": "contact@johngrattan.com",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "30 Roosevelt Road",
+      "addressLocality": "Plymouth",
+      "addressRegion": "MA",
+      "postalCode": "02360",
     },
-  };
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": "41.9359701",
+      "longitude": "-70.7228474",
+    },
+    "hasMap": "https://www.google.com/maps/place/John+Grattan+SEO+%26+Web+Design/@41.9359701,-70.7228474,15z/data=!4m5!3m4!1s0x0:0xbffcbd1521f630a0!8m2!3d41.9359701!4d-70.7228474",
+    "openingHours": "Mo, Tu, We, Th, Fr 09:00-17:00",
+    "contactPoint": {
+      "@type": "ContactPoint",
+      "contactType": "customer support",
+      "telephone": "+1-401 216 9868"
+    },
+    "url": "https://johngrattan.com/",
+  }`;
 
   return (
     <Helmet
@@ -49,41 +66,22 @@ function SEO({ description, lang, meta, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
+    >
+      <meta name="description" content={metaDescription} />
+
+      {/* Open Graph tags */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Card tags */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={site.siteMetadata.author} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
+
+      <script type="application/ld+json">{structuredDataLocalBusiness}</script>
+    </Helmet>
   );
 }
 
