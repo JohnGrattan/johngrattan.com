295d71104fbd1bbd4ff47b4bf8a61423558f2b50
John Grattan <jgrattan23@gmail.com>
2020-02-25 16:12:43 -0500

    Switched to MDX, updated Blog Post design

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,6 @@ module.exports = {
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-mdx`,
-    `gatsby-plugin-feed-mdx`,
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
