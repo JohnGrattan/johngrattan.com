115147d31afce49a0ee2eb0e54606f2d23543c2d
John Grattan <jgrattan23@gmail.com>
2020-05-13 11:53:22 -0400

    Finished Bitrix integration in contact form

--- src/components/FormContactBitrix/FormContactBitrix.js
+++ src/components/FormContactBitrix/FormContactBitrix.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import { navigate } from 'gatsby';
 import PropTypes from 'prop-types';
 import { Form, Button, Col } from 'react-bootstrap';
 
@@ -34,47 +34,98 @@ const FormContactBitrix = ({ formname }) => {
   // const [lastName, setLastName] = useState('');
   // const [phoneNumber, setPhoneNumber] = useState('');
   // const [email, setEmail] = useState('');
+  // const [website, setWebsite] = useState('');
   // const [company, setCompany] = useState('');
   // const [jobTitle, setJobTitle] = useState('');
+  // const [availability, setAvailability] = useState('');
   // const [service, setService] = useState('');
   // const [budget, setBudget] = useState('');
 
-  // const handleChange = e => {
+  // const handleFirstName = e => {
   //   setFirstName(e.target.value);
+  // };
+
+  // const handleLastName = e => {
   //   setLastName(e.target.value);
+  // };
+
+  // const handlePhoneNumber = e => {
   //   setPhoneNumber(e.target.value);
+  // };
+
+  // const handleEmail = e => {
   //   setEmail(e.target.value);
+  // };
+
+  // const handleWebsite = e => {
+  //   setWebsite(e.target.value);
+  // };
+
+  // const handleCompany = e => {
   //   setCompany(e.target.value);
+  // };
+
+  // const handleJobTitle = e => {
   //   setJobTitle(e.target.value);
+  // };
+
+  // const handleAvailability = e => {
+  //   setAvailability(e.target.value);
+  // };
+
+  // const handleService = e => {
   //   setService(e.target.value);
+  // };
+
+  // const handleBudget = e => {
   //   setBudget(e.target.value);
   // };
 
-  const handleSubmit = async (e, contact) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    await axios.post(
-      'https://b24-u57qin.bitrix24.com/rest/1/mtja2mf3e2o0r6s9/crm.lead.add',
-      {
-        data: {
+    await axios.all([
+      axios.post(
+        'https://b24-u57qin.bitrix24.com/rest/1/mtja2mf3e2o0r6s9/crm.lead.add',
+        {
           fields: {
-            TITLE: `${contact.firstName} ${contact.lastName}`,
-            NAME: `${contact.firstName} ${contact.lastName}`,
-            SECOND_NAME: `${contact.lastName}`,
-            PHONE: `${contact.phoneNumber}`,
-            EMAIL: `${contact.email}`,
-            COMPANY_TITLE: `${contact.company}`,
-            POSITION: `${contact.jobTitle}`,
+            TITLE: `${company} - ${service}`,
+            NAME: `${firstName}`,
+            LAST_NAME: `${lastName}`,
+            SECOND_NAME: `${firstName}`,
+            PHONE: [{ VALUE: `${phoneNumber}`, VALUE_TYPE: 'WORK' }],
+            EMAIL: [{ VALUE: `${email}`, VALUE_TYPE: 'WORK' }],
+            COMPANY_TITLE: `${company}`,
+            POST: `${jobTitle}`,
+            CURRENCY_ID: 'USD',
+            OPPORTUNITY: `${budget}`,
+            COMMENTS: `
+            This lead came from my contact form.<br/><br/>
+            <strong>Name</strong>: ${firstName} ${lastName}<br/>
+            <strong>Phone</strong>: ${phoneNumber}<br/>
+            <strong>Email</strong>: ${email}<br/>
+            <strong>Company</strong>: ${company}<br/>
+            <strong>Job Title</strong>: ${jobTitle}<br/>
+            <strong>Service</strong>: ${service}<br/>
+            <strong>Budget</strong>: ${budget}<br/><br/><br/>
+            <strong>Budget Ranges</strong>:<br/><br/>
+            $500 - $1,000<br/>
+            $1,000 - $2,000<br/>
+            $2,000 - $5,000<br/>
+            $5,000 - $10,000<br/>
+            $10,000+<br/>
+            `,
           },
-        },
-      }
-    );
+        }
+      ),
+    ]);
+    navigate('/thanks/');
   };
 
   return (
     <Form
       name={formname}
       method="POST"
-      action="/thanks/"
+      // action="/thanks/"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
       className="border border-secondary rounded shadow p-2 p-md-5 bg-texture-2"
@@ -95,7 +146,6 @@ const FormContactBitrix = ({ formname }) => {
             required
           />
         </Form.Group>
-
         <Form.Group as={Col} controlId="formContactBixtrixLastName">
           <Form.Label>
             Last Name <span className="text-danger">*</span>
@@ -109,6 +159,7 @@ const FormContactBitrix = ({ formname }) => {
           />
         </Form.Group>
       </Form.Row>
+
       <Form.Row>
         <Form.Group as={Col} controlId="formContactBixtrixPhone">
           <Form.Label>
@@ -149,7 +200,6 @@ const FormContactBitrix = ({ formname }) => {
             required
           />
         </Form.Group>
-
         <Form.Group as={Col} controlId="formContactBixtrixJobTitle">
           <Form.Label>
             Job Title <span className="text-danger">*</span>
@@ -172,7 +222,6 @@ const FormContactBitrix = ({ formname }) => {
           <Form.Control
             name="service"
             as="select"
-            // defaultValue={''}
             value={service}
             onChange={handleChange}
             required
@@ -180,20 +229,22 @@ const FormContactBitrix = ({ formname }) => {
             <option value="" disabled hidden>
               Choose One...
             </option>
-            <option value="0">SEO Site Audit</option>
-            <option value="1" className="bg-light">
-              Full-Service SEO
+            <option value="SEO: Site Audit">SEO: Site Audit</option>
+            <option value="SEO: Full-Service" className="bg-light">
+              SEO: Full-Service
             </option>
-            <option value="2">Build New Website</option>
-            <option value="3" className="bg-light">
-              Redesign Website
+            <option value="Website: New Build">Website: New Build</option>
+            <option value="Website: Redesign" className="bg-light">
+              Website: Redesign
             </option>
-            <option value="4">Pay-Per-Click Ads (PPC)</option>
-            <option value="5" className="bg-light">
-              Social Media Marketing
+            <option value="Marketing: PPC Ads">Marketing: PPC Ads</option>
+            <option value="Marketing: Social Media" className="bg-light">
+              Marketing: Social Media
             </option>
-            <option value="6">Email Marketing</option>
-            <option value="7" className="bg-light">
+            <option value="Marketing: Email Automation">
+              Marketing: Email Automation
+            </option>
+            <option value="SEO + Website + Marketing" className="bg-light">
               SEO + Website + Marketing
             </option>
           </Form.Control>
@@ -206,7 +257,6 @@ const FormContactBitrix = ({ formname }) => {
           <Form.Control
             name="budget"
             as="select"
-            // defaultValue={''}
             value={budget}
             onChange={handleChange}
             required
@@ -214,15 +264,15 @@ const FormContactBitrix = ({ formname }) => {
             <option value="" disabled hidden>
               Choose One...
             </option>
-            <option value="0">$500 - $1,000</option>
-            <option value="1" className="bg-light">
+            <option value="500.00">$500 - $1,000</option>
+            <option value="1000.00" className="bg-light">
               $1,000 - $2,000
             </option>
-            <option value="2">$2,000 - $5,000</option>
-            <option value="3" className="bg-light">
+            <option value="2000.00">$2,000 - $5,000</option>
+            <option value="5000.00" className="bg-light">
               $5,000 - $10,000
             </option>
-            <option value="4">$10,000+</option>
+            <option value="10000.00">$10,000+</option>
           </Form.Control>
         </Form.Group>
       </Form.Row>
@@ -231,15 +281,16 @@ const FormContactBitrix = ({ formname }) => {
       <Form.Text className="text-muted small mb-3">
         Your information will never be shared with anyone else.
       </Form.Text>
-
-      <Button
-        className="btn btn-primary btn-xl drop-shadow-dark font-weight-bold mt-3"
-        variant="primary"
-        type="submit"
-        value="Submit"
-      >
-        Get More Leads
-      </Button>
+      <div className="text-center">
+        <Button
+          className="btn btn-primary btn-xl drop-shadow-dark font-weight-bold mt-4"
+          variant="primary"
+          type="submit"
+          value="Submit"
+        >
+          Get More Leads
+        </Button>
+      </div>
     </Form>
   );
 };
