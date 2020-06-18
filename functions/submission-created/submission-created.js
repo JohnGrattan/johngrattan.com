770a02516ed7757900ce495d7d001a0364726a44
John Grattan <contact@johngrattan.com>
2020-06-18 15:32:08 -0400

    Updating function code

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,60 +1,3 @@
-// if (!process.env.NETLIFY) {
-//   // get local env vars if not in CI
-//   // if in CI i expect its already set via the Netlify UI
-//   require('dotenv').config();
-// }
-// required env vars
-// if (!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)
-//   throw new Error('no GOOGLE_SERVICE_ACCOUNT_EMAIL env var set');
-// if (!process.env.GOOGLE_PRIVATE_KEY)
-//   throw new Error('no GOOGLE_PRIVATE_KEY env var set');
-// if (!process.env.GOOGLE_SPREADSHEET_ID_FROM_URL)
-//   // spreadsheet key is the long id in the sheets URL
-//   throw new Error('no GOOGLE_SPREADSHEET_ID_FROM_URL env var set');
-
-// // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
-// const fetch = require('node-fetch');
-// const { EMAIL_TOKEN } = process.env;
-// exports.handler = async event => {
-//   // const firstName = JSON.parse(event.body).payload.firstName;
-//   // const lastName = JSON.parse(event.body).payload.lastName;
-//   // const phoneNumber = JSON.parse(event.body).payload.phoneNumber;
-//   const email = JSON.parse(event.body).payload.email;
-//   // const company = JSON.parse(event.body).payload.company;
-//   // const jobTitle = JSON.parse(event.body).payload.jobTitle;
-//   // const service = JSON.parse(event.body).payload.service;
-//   // const budget = JSON.parse(event.body).payload.budget;
-
-//   // const data = {
-//   //   firstName,
-//   //   lastName,
-//   //   phoneNumber,
-//   //   email,
-//   //   company,
-//   //   jobTitle,
-//   //   service,
-//   //   budget,
-//   // };
-
-//   console.log(`Recieved a submission: ${email}`);
-//   return fetch(
-//     'https://script.google.com/macros/s/AKfycbzoXD8zaE7kbfrVTWT-_yXjwz5S44e6MQLuEzgPNLgbcvfsDf0/exec',
-//     {
-//       method: 'POST',
-//       headers: {
-//         // Authorization: `Token ${EMAIL_TOKEN}`,
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ data }),
-//     }
-//   )
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(`Submitted to Google Sheets:\n ${email}`);
-//     })
-//     .catch(error => ({ statusCode: 422, body: String(error) }));
-// };
-
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const {
   GATSBY_GOOGLE_SERVICE_ACCOUNT_EMAIL,
@@ -64,17 +7,7 @@ const {
 
 exports.handler = async (event, context, callback) => {
   try {
-    const firstName = JSON.parse(event.body).payload.firstName;
-    const lastName = JSON.parse(event.body).payload.lastName;
-    const phoneNumber = JSON.parse(event.body).payload.phoneNumber;
-    const email = JSON.parse(event.body).payload.email;
-    const company = JSON.parse(event.body).payload.company;
-    const jobTitle = JSON.parse(event.body).payload.jobTitle;
-    const service = JSON.parse(event.body).payload.service;
-    const budget = JSON.parse(event.body).payload.budget;
-    console.log(
-      `Received a submission: ${firstName}, ${lastName}, ${phoneNumber}, ${email}, ${company}, ${jobTitle}, ${service}, ${budget}`
-    );
+    console.log(`Received a submission: ${data}`);
 
     const doc = new GoogleSpreadsheet({
       GATSBY_GOOGLE_SPREADSHEET_ID_FROM_URL,
@@ -86,31 +19,21 @@ exports.handler = async (event, context, callback) => {
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
 
-    // const data = JSON.parse(event.body).payload.email;
-    console.log(
-      `Submission Info:\n ${firstName}, ${lastName}, ${phoneNumber}, ${email}, ${company}, ${jobTitle}, ${service}, ${budget}`
-    );
+    console.log(`Submission Info: ${data}`);
 
-    const addedRow = await sheet.addRow([
-      { firstName: { firstName } },
-      { lastName: { lastName } },
-      { phoneNumber: { phoneNumber } },
-      { email: { email } },
-      { company: { company } },
-      { jobTitle: { jobTitle } },
-      { service: { service } },
-      { budget: { budget } },
-    ]);
+    const data = JSON.parse(event.body);
+    const addedRow = await sheet.addRow(data);
 
-    return callback(null, {
+    return {
       statusCode: 200,
-      body: JSON.stringify(firstName),
-    });
-  } catch (error) {
-    console.log(error);
-    return callback(null, {
+      body: JSON.stringify({
+        message: `row added`,
+      }),
+    };
+  } catch (e) {
+    return {
       statusCode: 500,
-      body: error.toString(),
-    });
+      body: e.toString(),
+    };
   }
 };
